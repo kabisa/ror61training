477276e42a3eae68891e1bf6434e2fe2b9d120ad
Patrick Baselier <patrick.baselier@gmail.com>
2022-06-07 13:32:26 +0200

    Add validation to Link model

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,2 +1,8 @@
+# app/models/link.rb
 class Link < ApplicationRecord
+  validates_presence_of :title
+  validates :url,
+            presence: true,
+            uniqueness: true,
+            format: { with: %r{https?://}i }
 end
