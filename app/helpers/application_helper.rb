5d93ff35c799d8f85100aa3b33157bc56a34c8eb
Patrick Baselier <patrick.baselier@gmail.com>
2022-06-07 12:23:14 +0200

    Extract logic to helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def company_name
+    'My company'
+  end
+
+  # human_change_time(__FILE__)
+  def human_change_time(file)
+    "#{time_ago_in_words(File.ctime(file))} ago"
+  end
+
+  # truncated_link_to(post, :title)
+  def truncated_link_to(object, attribute)
+    text = object.send(attribute)
+    link_to(truncate(text), object, title: text)
+  end
 end
