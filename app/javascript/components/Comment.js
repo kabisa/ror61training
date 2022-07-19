7ba12c2143595393235a623370d0b0feaf995dac
Jordy van Vorselen <jordyvanvorselen@gmail.com>
2022-07-19 16:41:02 +0200

    Add a PropType for the text prop

--- app/javascript/components/Comment.js
+++ app/javascript/components/Comment.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Comment = ({ text }) => {
   return <p>Hello {text}!</p>
+}
+
+Comment.propTypes = {
+  text: PropTypes.string.isRequired
 }
 
 export default Comment;
