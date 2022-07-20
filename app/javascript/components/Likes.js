3dab0f08606bf8b9d86dac0d66f3802115cb9423
Jordy van Vorselen <jordyvanvorselen@gmail.com>
2022-07-20 10:22:07 +0200

    Extract likes to separate component

---
+++ app/javascript/components/Likes.js
@@ -0,0 +1,13 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Likes = () => {
+  return (
+    <div className="likes">
+      <i className="fas fa-heart"></i>
+      <p>1</p>
+    </div>
+  );
+};
+
+export default Likes;
