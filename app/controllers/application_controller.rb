3d1d4f350ce0f5dba6c9c1a115f2d9ddbf575ad1
Jordy van Vorselen <jordyvanvorselen@gmail.com>
2022-07-27 12:23:23 +0200

    Save uuid in cookie if not already set

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   around_action :switch_locale
+  before_action :set_uuid, unless: :uuid_already_set?
 
   def default_url_options
     { locale: I18n.locale }
@@ -10,5 +11,13 @@ class ApplicationController < ActionController::Base
   def switch_locale(&action)
     locale = params[:locale] || I18n.default_locale
     I18n.with_locale(locale, &action)
+  end
+
+  def set_uuid
+    cookies[:uuid] = SecureRandom.uuid
+  end
+
+  def uuid_already_set?
+    cookies[:uuid].present?
   end
 end
